dbd9057db3238e376122f7ab030e4052a8817148
kenedi novriansyah7 <kenedi@rubyh.co>
2022-07-30 11:10:43 +0700

    setup antd

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,13 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import AppRoutingModule from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+import { DemoNgZorroAntdModule } from './ng-zorro.module';
+import en from '@angular/common/locales/en';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { HomeComponent } from './pages/home/home.component';
+
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  (key) => antDesignIcons[key]
+);
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    NavbarComponent,
+    ButtonComponent,
+    InputComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DemoNgZorroAntdModule,
+    DragDropModule,
+    ScrollingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_ICONS, useValue: icons },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
