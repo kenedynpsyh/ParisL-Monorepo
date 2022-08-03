c25232c46b47283283c9164961536f57848abda9
kenedi novriansyah7 <kenedi@rubyh.co>
2022-08-04 05:49:00 +0700

    fix

--- apps/app/src/app/app.component.spec.ts
+++ apps/app/src/app/app.component.spec.ts
@@ -1,14 +1,28 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { UserEffect } from './services/effects/user-effect';
 import { PrefixComponentModule } from './services/prefix/prefix-component.module';
 import { UIComponentModule } from './services/prefix/ui-component';
+import { messageReducer } from './store/reducers/message-reducer';
+import { userReducer } from './store/reducers/user-reducer';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, UIComponentModule, PrefixComponentModule],
+      imports: [
+        RouterTestingModule,
+        UIComponentModule,
+        PrefixComponentModule,
+        StoreModule.forRoot({
+          user: userReducer,
+          message: messageReducer,
+        }),
+        EffectsModule.forRoot([UserEffect]),
+      ],
       declarations: [AppComponent],
       providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     }).compileComponents();
